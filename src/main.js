0421f33caf040c35fd00b51e19a996db88e66ac4
arun-kumar <arun.kumar@hpe.com>
2023-03-13 15:18:23 +0530

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import store from "./store"
+import router from "./router"
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .use(store)
+  .use(router)
+  .mount('#app')
+
