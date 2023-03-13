4aa92afe95ce584a21734515efb5d4ac5ced2800
arun-kumar <arun.kumar@hpe.com>
2023-03-13 18:59:36 +0530

    u[dtaes

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,24 @@
-console.log("")
+import { createRouter, createWebHistory } from 'vue-router'
+import PostView from '../views/PostView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
+const routes = [
+  {
+    path: '/view-posts',
+    name: 'PostViews',
+    component: PostView
+  },
+  // 404 catchall
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFoundView',
+    component: NotFoundView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,31 @@
-console.log("")
+import { createStore } from "vuex";
+import axios from "axios";
+
+const store = createStore({
+    state:{
+        posts: [],
+    },
+    getters:{
+        getPosts: (state) => state.posts,
+    },
+    mutations:{
+        SET_POSTS(state, posts) {
+            state.posts = posts;
+        },
+    },
+    actions:{
+        async getPosts({ commit }) {
+            try {
+              const data = await axios.get(
+                "http://localhost:3000/posts"
+              );
+              commit("SET_POSTS", data.data);
+            } catch (error) {
+              alert(error);
+              console.log(error);
+            }
+        },
+    }
+});
+
+export default store;
